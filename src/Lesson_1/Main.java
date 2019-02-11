59d12831227e51f029d44399b1d3a02b29096400
Andrii Fedosov <shinkun1993@gmail.com>
2019-02-11 19:32:43 +0200

    add pom.xm and first lesson

---
+++ src/Lesson_1/Main.java
@@ -0,0 +1,8 @@
+package Lesson_1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Obi-Van:Hello there!");
+        System.out.println("General Grievous: General Kenobi.");
+    }
+}
