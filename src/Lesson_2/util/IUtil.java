bc6769d98438ad02b77ce8540a4cb4fb25e523e4
Andrii Fedosov <shinkun1993@gmail.com>
2019-03-13 20:38:45 +0200

    commit

---
+++ src/Lesson_2/util/IUtil.java
@@ -0,0 +1,12 @@
+package Lesson_2.util;
+
+public interface IUtil {
+    int factorial(int count);
+    int rate(int number, int rate);
+    void increment(int number);
+    void decrement(int number);
+    void fibonachiRow();
+    void concetinationString();
+
+
+}
---
+++ src/Lesson_2/util/UtilImpl.java
@@ -0,0 +1,28 @@
+package Lesson_2.util;
+
+public class UtilImpl implements IUtil {
+
+    public int factorial(int count) {
+        return 0;
+    }
+
+    public int rate(int number, int rate) {
+        return 0;
+    }
+
+    public void increment(int number) {
+
+    }
+
+    public void decrement(int number) {
+
+    }
+
+    public void fibonachiRow() {
+
+    }
+
+    public void concetinationString() {
+
+    }
+}
