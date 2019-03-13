48935ed33cc0a88541ba9eb52d0dc0a852472492
Andrii Fedosov <shinkun1993@gmail.com>
2019-03-13 20:24:57 +0200

    add class car

---
+++ src/Lesson_1/Car.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+public class Car {
+    String color;
+    String name;
+
+    public  void  run(){
+        System.out.println("ruuuun!!!!!");
+    }
+}
--- src/Lesson_1/Main.java
+++ src/Lesson_1/Main.java
@@ -4,5 +4,19 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Obi-Van:Hello there!");
         System.out.println("General Grievous: General Kenobi.");
+        Car car1 = new Car();
+        car1.run();
+        String s = null;
+        System.out.println("s = " + s);
+        char c = 'c';
+        System.out.println("one two");
+        System.out.println("one\n two");
+        System.out.println("c = " + c);
+        int[] arrInt = {120, 12, 3, 5};
+        arrInt[0] = 42;
+        System.out.println("arrInt[0] = " + arrInt[0]);
+        System.out.println("arrInt[1] = " + arrInt[1]);
+        System.out.println("arrInt[2] = " + arrInt[2]);
+        System.out.println("arrInt[3] = " + arrInt[3]);
     }
 }
