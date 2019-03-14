906d2d43f82acea9f68c00333a2607e67bf5b98b
Andrii Fedosov <shinkun1993@gmail.com>
2019-03-14 10:09:58 +0200

    commit

--- src/Lesson_2/Main.java
+++ src/Lesson_2/Main.java
@@ -1,7 +1,10 @@
 package Lesson_2;
 
+import Lesson_2.util.UtilImpl;
+
 public class Main {
     public static void main(String[] args) {
-
+        UtilImpl util = new UtilImpl();
+        util.reverseTree();
     }
 }
--- src/Lesson_2/util/UtilImpl.java
+++ src/Lesson_2/util/UtilImpl.java
@@ -25,4 +25,44 @@ public class UtilImpl implements IUtil {
     public void concetinationString() {
 
     }
+
+    public void multiplyTable() {
+        for (int firsElement = 1; firsElement <= 10; firsElement++) {
+            for (int secondElement = 1; secondElement <= 10; secondElement++) {
+                System.out.print(firsElement + "x" + secondElement + "=" + firsElement * secondElement + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public void multiplyTableVertical() {
+        for (int firsElement = 1; firsElement <= 10; firsElement++) {
+            for (int secondElement = 1; secondElement <= 10; secondElement++) {
+                System.out.print(secondElement + "x" + firsElement + "=" + firsElement * secondElement + " ");
+            }
+            System.out.println("\n");
+        }
+    }
+
+    public void tree() {
+
+        for (int mainIndex = 0; mainIndex < 10; mainIndex++) {
+            for (int firstIndex = 0; firstIndex < 10 - mainIndex; firstIndex++)
+                System.out.print(" ");
+            for (int secondIndex = 0; secondIndex < (2 * mainIndex + 1); secondIndex++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
+    public void reverseTree(){
+        for (int mainIndex = 10; mainIndex >= 0; mainIndex--) {
+            for (int firstIndex = 0; firstIndex < 10 - mainIndex; firstIndex++)
+                System.out.print(" ");
+            for (int secondIndex = 0; secondIndex < (2 * mainIndex + 1); secondIndex++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
 }
